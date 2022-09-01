7e240aa6b2752440f1447c077947d82a1ed68964
n6430 <n643064@tutanota.com>
2022-09-01 17:01:04 +0200

    Switch to fabric API.

--- src/main/java/n643064/snowed/Snowed.java
+++ src/main/java/n643064/snowed/Snowed.java
@@ -1,12 +1,41 @@
 package n643064.snowed;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModificationContext;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.SpawnSettings;
+
+import static net.minecraft.world.dimension.DimensionOptions.OVERWORLD;
 
 public class Snowed implements ModInitializer
 {
     @Override
     public void onInitialize()
     {
+        BiomeModifications.create(new Identifier("snowed:freeze_world")).add(ModificationPhase.REPLACEMENTS, ctx -> ctx.canGenerateIn(OVERWORLD), ctx ->
+        {
+            final BiomeModificationContext.WeatherContext weather = ctx.getWeather();
+            final BiomeModificationContext.SpawnSettingsContext spawnSettings = ctx.getSpawnSettings();
+            final BiomeModificationContext.EffectsContext effects = ctx.getEffects();
+
+            weather.setPrecipitation(Biome.Precipitation.SNOW);
+            weather.setTemperatureModifier(Biome.TemperatureModifier.NONE);
+
+            effects.setSkyColor(0xE6EAFF);
+            effects.setFoliageColor(0x57a36d);
+            effects.setGrassColor(0x57a36d);
+
+            spawnSettings.addSpawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(EntityType.FOX, 50, 1, 3));
+            spawnSettings.addSpawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(EntityType.POLAR_BEAR, 40, 1, 3));
+            spawnSettings.addSpawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(EntityType.SNOW_GOLEM, 10, 3, 6));
+            spawnSettings.addSpawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityType.STRAY, 65, 1, 3));
+
+        });
 
     }
 }
--- src/main/java/n643064/snowed/mixin/BiomeMixin.java
+++ src/main/java/n643064/snowed/mixin/BiomeMixin.java
@@ -2,7 +2,6 @@ package n643064.snowed.mixin;
 
 import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -11,35 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Biome.class)
 public abstract class BiomeMixin
 {
-
-    @Inject(method = "getPrecipitation", at = @At("HEAD"), cancellable = true)
-    private void getPrecipitation(CallbackInfoReturnable<Biome.Precipitation> cir)
-    {
-        cir.setReturnValue(Biome.Precipitation.SNOW);
-    }
-
     @Inject(method = "getTemperature()F", at = @At("RETURN"), cancellable = true)
     private void getTemperature(CallbackInfoReturnable<Float> cir)
     {
         float temp = cir.getReturnValue();
-        cir.setReturnValue(temp > 1.5f ? 0 : temp - 1.5f);
-    }
-
-    @Inject(method = "getSkyColor", at = @At("HEAD"), cancellable = true)
-    private void getSkyColor(CallbackInfoReturnable<Integer> cir)
-    {
-        cir.setReturnValue(0xE6EAFF);
-    }
-
-    @Inject(method = "getFoliageColor", at = @At("HEAD"), cancellable = true)
-    private void getDefaultFoliageColor(CallbackInfoReturnable<Integer> cir)
-    {
-        cir.setReturnValue(0x57a36d);
-    }
-
-    @Inject(method = "getDefaultGrassColor", at = @At("HEAD"), cancellable = true)
-    private void getDefaultGrassColor(CallbackInfoReturnable<Integer> cir)
-    {
-        cir.setReturnValue(0x57a36d);
+        cir.setReturnValue(temp > 1.5f ? 0 : temp - 1.6f);
     }
 }
